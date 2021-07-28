ed15532a47974337294b60b724a90599f75801b3
Duy Huynh <hvnduy@hotmail.com>
2021-07-28 15:58:57 +0700

    update cors

--- src/main/java/com/javainuse/config/CorsConfiguration.java
+++ src/main/java/com/javainuse/config/CorsConfiguration.java
@@ -15,7 +15,7 @@ public class CorsConfiguration {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(allowedOrigins);
+				registry.addMapping("/**");
 			}
 		};
 	}
